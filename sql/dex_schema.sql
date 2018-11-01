fce273205d820ef3e911cfce2c4642c028fed322
Jesse Pirnat <jessepirnat@gmail.com>
2018-11-01 12:42:36 -0400

    Adding table to define TMs and HMs.

--- sql/dex_schema.sql
+++ sql/dex_schema.sql
@@ -841,6 +841,32 @@ foreign key (`flag_id`) references `flags` (`id`)
 ) engine = InnoDB;
 
 
+create table if not exists `technical_machines`
+(
+`version_group_id` tinyint unsigned not null,
+`is_hidden` bool not null,
+`number` tinyint unsigned not null,
+
+`item_id` smallint unsigned not null,
+`move_id` smallint unsigned not null,
+
+primary key (
+	`version_group_id`,
+	`is_hidden`,
+	`number`
+),
+foreign key (`version_group_id`) references `version_groups` (`id`)
+	on delete restrict
+	on update cascade,
+foreign key (`item_id`) references `items` (`id`)
+	on delete restrict
+	on update cascade,
+foreign key (`move_id`) references `moves` (`id`)
+	on delete restrict
+	on update cascade
+) engine = InnoDB;
+
+
 create table if not exists `move_methods`
 (
 `id` tinyint unsigned not null,
