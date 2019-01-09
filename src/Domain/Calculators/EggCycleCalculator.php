35d8fb3a59de2a2fb1ee78716d902a6a27f9f760
Jesse Pirnat <jessepirnat@gmail.com>
2019-01-09 16:09:56 -0500

    Egg cycle calculator to calculate steps to hatch eggs in each generation.

---
+++ src/Domain/Calculators/EggCycleCalculator.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace Jp\Dex\Domain\Calculators;
+
+use Jp\Dex\Domain\Versions\GenerationId;
+use Jp\Dex\Domain\Versions\GenerationNotFoundException;
+
+class EggCycleCalculator
+{
+	/**
+	 * Calculate the number of steps it would take for an egg to hatch.
+	 *
+	 * @param GenerationId $generationId
+	 * @param int $baseEggCycles
+	 * @param bool $magmaArmor
+	 * @param string $oPower '', 'weak', 'medium', or 'strong'.
+	 *
+	 * @throws GenerationNotFoundException if steps to hatch cannot be calculated
+	 *     for $generationId.
+	 *
+	 * @return int
+	 */
+	public function calculateStepsToHatch(
+		GenerationId $generationId,
+		int $baseEggCycles,
+		bool $magmaArmor,
+		string $oPower
+	) : int {
+		$generation = $generationId->value();
+
+		$stepsPerCycle = [
+			2 => 256,
+			3 => 256,
+			4 => 255,
+			5 => 257,
+			6 => 257,
+			7 => 257,
+		][$generation];
+
+		$magmaArmor = $magmaArmor ? 2 : 1;
+
+		if ($oPower && 5 <= $generation && $generation <= 7) {
+			$stepsPerCycle = [
+				'weak' => 205,
+				'medium' => 171,
+				'strong' => 129,
+			][$oPower];
+		}
+
+		if ($generation === 2) {
+			return $baseEggCycles * $stepsPerCycle;
+		}
+		if ($generation === 3) {
+			return (ceil($baseEggCycles / $magmaArmor) + 1) * $stepsPerCycle - 1;
+		}
+		if ($generation === 4) {
+			return (ceil($baseEggCycles / $magmaArmor) + 1) * $stepsPerCycle;
+		}
+		if ($generation === 5) {
+			return ceil($baseEggCycles / $magmaArmor) * $stepsPerCycle;
+		}
+		if ($generation === 6) {
+			return ceil($baseEggCycles / $magmaArmor) * $stepsPerCycle;
+		}
+		if ($generation === 7) {
+			return ceil($baseEggCycles / $magmaArmor) * $stepsPerCycle;
+		}
+
+		throw new GenerationNotFoundException(
+			"Cannot calculate steps to hatch for generation id $generation."
+		);
+	}
+}
