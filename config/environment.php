fc13d794fe01439b12e27d2c38e2018f384af105
Jesse Pirnat <jessepirnat@gmail.com>
2019-03-05 15:35:37 -0500

    Formats for 2019-02.

--- config/environment.php
+++ config/environment.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 // If we're not on production, our environment variables may not be set.
 // In that case, we need to load them into the environment.
 if (getenv('ENVIRONMENT') !== 'production') {
-	$dotenv = \Dotenv\Dotenv::create(__DIR__ );
+	$dotenv = \Dotenv\Dotenv::create(__DIR__);
 	$dotenv->load();
 }
