ba3bd510d5596dee2b29c76a0c6bd83c509c2b1e
Jesse Pirnat <jessepirnat@gmail.com>
2020-02-07 13:17:23 -0500

    Added catch rate data for Let's Go, and changed it from per species_id to per pokemon_id (because of the Partner Pokémon).

--- sql/schema/dex/catch_rates.sql
+++ sql/schema/dex/catch_rates.sql
@@ -1,18 +1,18 @@
 create table if not exists `catch_rates`
 (
 `version_group_id` tinyint unsigned not null,
-`species_id` smallint unsigned not null,
+`pokemon_id` smallint unsigned not null,
 
 `catch_rate` tinyint unsigned not null,
 
 primary key (
 	`version_group_id`,
-	`species_id`
+	`pokemon_id`
 ),
-foreign key (`species_id`) references `species` (`id`)
+foreign key (`version_group_id`) references `version_groups` (`id`)
 	on delete restrict
 	on update cascade,
-foreign key (`version_group_id`) references `version_groups` (`id`)
+foreign key (`pokemon_id`) references `pokemon` (`id`)
 	on delete restrict
 	on update cascade
 ) engine = InnoDB;
