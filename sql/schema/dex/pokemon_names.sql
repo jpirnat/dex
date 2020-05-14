70395ededfc79a7f9c18cc9ed12373a8fb7d8642
Jesse Pirnat <jessepirnat@gmail.com>
2020-05-14 10:37:31 -0400

    Formats for 2019-12.

--- sql/schema/dex/pokemon_names.sql
+++ sql/schema/dex/pokemon_names.sql
@@ -3,8 +3,8 @@ create table if not exists `pokemon_names`
 `language_id` tinyint unsigned not null,
 `pokemon_id` smallint unsigned not null,
 
-`name` varchar(31) not null,
-`category` varchar(21) not null,
+`name` varchar(50) not null,
+`category` varchar(30) not null,
 
 primary key (
 	`language_id`,
