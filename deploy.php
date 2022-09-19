f723a6c7a94bd9f3e4fdefe2886459cc03f66b95
Jesse Pirnat <jessepirnat@gmail.com>
2022-09-19 14:46:51 -0400

    Updating deployer from v6 to v7.

--- deploy.php
+++ deploy.php
@@ -3,50 +3,40 @@ namespace Deployer;
 
 require 'recipe/common.php';
 
-// Project name
-set('application', 'dex');
+// Config
 
-// Project repository
 set('repository', 'https://jpirnat@github.com/jpirnat/dex.git');
 
-// [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true);
-
-// Shared files/dirs between deploys
-set('shared_files', []);
-set('shared_dirs', []);
-
-// Writable dirs by web server
-set('writable_dirs', ['config/cache', 'templates/cache']);
-set('allow_anonymous_stats', false);
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', ['config/cache', 'templates/cache']);
 
 // Hosts
 
 host('159.89.92.65')
-	->user('jpirnat')
-	->stage('production')
+	->set('remote_user', 'jpirnat')
+	->set('labels', ['stage' => 'production'])
 	->set('deploy_path', '/var/www/dex')
 	->set('branch', 'master')
 ;
+/*
+Add the following to your computer's ~/.ssh/config file:
+Host 159.89.92.65
+	HostName 159.89.92.65
+	User jpirnat
+	IdentityFile ~/.ssh/PRIVATE_KEY_NAME
 
+And on the server, add the public key in ~/.ssh/authorized_keys
+*/
 
 // Tasks
 
 desc('Deploy your project');
 task('deploy', [
-    'deploy:info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:vendors',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success'
+	'deploy:prepare',
+	'deploy:vendors',
+	'deploy:clear_paths',
+	'deploy:publish',
 ]);
 
 task('reload:php-fpm', function () {
@@ -57,8 +47,9 @@ task('reload:php-fpm', function () {
 // the updated command can be run without a password:
 // jpirnat ALL=NOPASSWD: /etc/init.d/php8.0-fpm restart
 
+// Hooks
+
 after('deploy', 'reload:php-fpm');
 after('rollback', 'reload:php-fpm');
 
-// [Optional] If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
