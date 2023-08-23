c76a30e81eece0273455ed961666441236212d08
Jesse Pirnat <jessepirnat@gmail.com>
2023-08-23 11:15:27 -0400

    Updated generations, version_groups, versions, version_group_names, and version_names for BDSP through SV.

--- sql/schema/dex/version_groups.sql
+++ sql/schema/dex/version_groups.sql
@@ -1,10 +1,10 @@
 create table if not exists `version_groups`
 (
 `id` tinyint unsigned not null,
-`identifier` varchar(8) not null,
+`identifier` varchar(31) not null,
 
 `generation_id` tinyint unsigned not null,
-`icon` varchar(29) not null,
+`icon` varchar(35) not null,
 `sort` tinyint unsigned not null,
 `breeding_priority` tinyint unsigned not null,
 
