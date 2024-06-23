2a2fdc2f729a4c8b3beb75055fe29284a18f6ab5
Jesse Pirnat <jessepirnat@gmail.com>
2024-06-23 11:50:29 -0400

    Added `version_group_id` to `formats`. (Will most likely replace the existing `generation_id` completely.)

--- sql/schema/dex/formats.sql
+++ sql/schema/dex/formats.sql
@@ -4,6 +4,7 @@ create table if not exists `formats`
 `identifier` varchar(30) not null,
 
 `generation_id` tinyint unsigned not null,
+`version_group_id` tinyint unsigned not null,
 `level` tinyint unsigned not null,
 `field_size` tinyint unsigned not null,
 `team_size` tinyint unsigned not null,
@@ -13,6 +14,9 @@ create table if not exists `formats`
 primary key (`id`),
 unique key (`identifier`),
 foreign key (`generation_id`) references `generations` (`id`)
+	on delete restrict
+	on update cascade,
+foreign key (`version_group_id`) references `version_groups` (`id`)
 	on delete restrict
 	on update cascade
 ) engine = InnoDB;
