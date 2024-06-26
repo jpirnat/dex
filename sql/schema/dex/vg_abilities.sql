c5bd451500e318c5f1fcba5565d132e77f74d7d6
Jesse Pirnat <jessepirnat@gmail.com>
2024-06-26 13:50:19 -0400

    Removed the ability flags from `vg_abilities`.

--- sql/schema/dex/vg_abilities.sql
+++ sql/schema/dex/vg_abilities.sql
@@ -3,16 +3,6 @@ create table if not exists `vg_abilities`
 `version_group_id` tinyint unsigned not null,
 `ability_id` smallint unsigned not null,
 
-`disabled_by_neutralizing_gas` bool null,
-`fail_role_play` bool null,
-`no_receiver` bool null,
-`no_entrain` bool null,
-`no_trace` bool null,
-`fail_skill_swap` bool null,
-`cant_suppress` bool null,
-`breakable` bool null,
-`no_transform` bool null,
-
 primary key (`version_group_id`, `ability_id`),
 foreign key (`version_group_id`) references `version_groups` (`id`)
 	on delete restrict
