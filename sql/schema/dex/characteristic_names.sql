65260228f0a9561df6f90a9e051a0dd3df4781aa
Jesse Pirnat <jessepirnat@gmail.com>
2024-09-17 18:38:04 -0400

    Added characteristic names per version group. This is in anticipation of working on an IV Calculator.

--- sql/schema/dex/characteristic_names.sql
+++ sql/schema/dex/characteristic_names.sql
@@ -1,14 +1,19 @@
 create table if not exists `characteristic_names`
 (
+`version_group_id` tinyint unsigned not null,
 `language_id` tinyint unsigned not null,
 `characteristic_id` tinyint unsigned not null,
 
-`name` varchar(40) not null,
+`name` varchar(44) not null,
 
 primary key (
+	`version_group_id`,
 	`language_id`,
 	`characteristic_id`
 ),
+foreign key (`version_group_id`) references `version_groups` (`id`)
+	on delete restrict
+	on update cascade,
 foreign key (`language_id`) references `languages` (`id`)
 	on delete restrict
 	on update cascade,
