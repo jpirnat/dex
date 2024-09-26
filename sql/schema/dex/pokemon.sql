742196c83ad3a56e46d9f31d378d28bea89d5d35
Jesse Pirnat <jessepirnat@gmail.com>
2024-09-26 19:14:31 -0400

    Fully removed the deleted database column pokemon.introduced_in_version_group_id

--- sql/schema/dex/pokemon.sql
+++ sql/schema/dex/pokemon.sql
@@ -6,7 +6,6 @@ create table if not exists `pokemon`
 
 `species_id` smallint unsigned not null,
 `is_default_pokemon` bool not null,
-`introduced_in_version_group_id` tinyint unsigned not null,
 `experience_group_id` tinyint unsigned not null,
 `gender_ratio` tinyint signed not null,
 `smogon_dex_identifier` varchar(20) not null,
@@ -15,9 +14,6 @@ create table if not exists `pokemon`
 primary key (`id`),
 unique key (`identifier`),
 foreign key (`species_id`) references `species` (`id`)
-	on delete restrict
-	on update cascade,
-foreign key (`introduced_in_version_group_id`) references `version_groups` (`id`)
 	on delete restrict
 	on update cascade,
 foreign key (`experience_group_id`) references `experience_groups` (`id`)
