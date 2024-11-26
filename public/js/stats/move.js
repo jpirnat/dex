0fa5ef3cce2df368f9fd1a0a4f43d9d73b41df9c
Jesse Pirnat <jessepirnat@gmail.com>
2024-11-26 11:31:22 -0500

    On stats/ability and stats/item and stats/move, maintain the name filter across the prev/next month and ratings nav.

--- public/js/stats/move.js
+++ public/js/stats/move.js
@@ -38,6 +38,13 @@ const app = createApp({
 		};
 	},
 	computed: {
+		queryParams() {
+			if (this.filterName) {
+				return `?name=${encodeURIComponent(this.filterName)}`;
+			}
+
+			return '';
+		},
 		filteredPokemons() {
 			let filteredPokemons = this.pokemons;
 
@@ -80,6 +87,11 @@ const app = createApp({
 				this.pokemons = data.pokemons;
 
 				document.title = data.title;
+			}
+
+			const filterName = url.searchParams.get('name');
+			if (filterName) {
+				this.filterName = filterName;
 			}
 		});
 	},
