644a8ed6b64b35c50805abb5262eb3015830b0f4
Jesse Pirnat <jessepirnat@gmail.com>
2025-01-02 16:40:28 -0500

    Using the new AbilityTypeMatchups class in DexPokemonMatchupsModel.

--- src/Application/Models/DexPokemon/DexPokemonMatchupsModel.php
+++ src/Application/Models/DexPokemon/DexPokemonMatchupsModel.php
@@ -3,16 +3,14 @@ declare(strict_types=1);
 
 namespace Jp\Dex\Application\Models\DexPokemon;
 
-use Jp\Dex\Domain\Abilities\AbilityIdentifier;
+use Jp\Dex\Domain\Abilities\AbilityTypeMatchups;
 use Jp\Dex\Domain\Abilities\ExpandedDexPokemonAbility;
 use Jp\Dex\Domain\Languages\LanguageId;
 use Jp\Dex\Domain\Pokemon\PokemonId;
 use Jp\Dex\Domain\Pokemon\VgPokemonRepositoryInterface;
 use Jp\Dex\Domain\Types\DexType;
 use Jp\Dex\Domain\Types\DexTypeRepositoryInterface;
-use Jp\Dex\Domain\Types\TypeIdentifier;
 use Jp\Dex\Domain\Types\TypeMatchupRepositoryInterface;
-use Jp\Dex\Domain\Versions\GenerationId;
 use Jp\Dex\Domain\Versions\VersionGroup;
 
 final class DexPokemonMatchupsModel
@@ -33,6 +31,7 @@ final class DexPokemonMatchupsModel
 		private readonly DexTypeRepositoryInterface $dexTypeRepository,
 		private readonly VgPokemonRepositoryInterface $vgPokemonRepository,
 		private readonly TypeMatchupRepositoryInterface $typeMatchupRepository,
+		private readonly AbilityTypeMatchups $abilityTypeMatchups,
 	) {}
 
 
@@ -81,7 +80,26 @@ final class DexPokemonMatchupsModel
 
 		if ($versionGroup->hasAbilities) {
 			foreach ($abilities as $ability) {
-				$this->checkForMatchups($versionGroup->generationId, $ability);
+				$hasMatchups = $this->abilityTypeMatchups->hasMatchups(
+					$versionGroup->generationId,
+					$ability->identifier,
+				);
+
+				if ($hasMatchups) {
+					$this->abilities[] = [
+						'identifier' => $ability->identifier,
+						'name' => $ability->name,
+					];
+
+					$abilityMultipliers = $this->abilityTypeMatchups->getMatchups(
+						$versionGroup->generationId,
+						$ability->identifier,
+						$this->damageTaken[self::NO_ABILITY],
+					);
+
+					$abilityIdentifier = $ability->identifier;
+					$this->damageTaken[$abilityIdentifier] = $abilityMultipliers;
+				}
 			}
 		}
 
@@ -91,166 +109,5 @@ final class DexPokemonMatchupsModel
 		];
 
 		$this->types = $allTypes;
-	}
-
-	/**
-	 * If this ability changes any of the Pokémon's matchups, add the ability
-	 * to the matchups array.
-	 */
-	private function checkForMatchups(GenerationId $generationId, ExpandedDexPokemonAbility $ability) : void
-	{
-		$abilityIdentifier = $ability->identifier;
-
-		if ($abilityIdentifier === AbilityIdentifier::VOLT_ABSORB) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::ELECTRIC] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::WATER_ABSORB) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::WATER] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::FLASH_FIRE) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::FIRE] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::WONDER_GUARD) {
-			$this->addToDamageTaken($ability);
-			foreach ($this->damageTaken[$abilityIdentifier] as $type => $multiplier) {
-				if ($multiplier <= 1) {
-					$this->damageTaken[$abilityIdentifier][$type] *= 0;
-				}
-			}
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::LEVITATE) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::GROUND] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::LIGHTNING_ROD && $generationId->value >= 5) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::ELECTRIC] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::THICK_FAT) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::FIRE] *= .5;
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::ICE] *= .5;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::MOTOR_DRIVE) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::ELECTRIC] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::HEATPROOF) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::FIRE] *= .5;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::DRY_SKIN) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::FIRE] *= 1.25;
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::WATER] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::FILTER) {
-			$this->addToDamageTaken($ability);
-			foreach ($this->damageTaken[$abilityIdentifier] as $type => $multiplier) {
-				if ($multiplier > 1) {
-					$this->damageTaken[$abilityIdentifier][$type] *= .75;
-				}
-			}
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::STORM_DRAIN && $generationId->value >= 5) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::WATER] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::SOLID_ROCK) {
-			$this->addToDamageTaken($ability);
-			foreach ($this->damageTaken[$abilityIdentifier] as $type => $multiplier) {
-				if ($multiplier > 1) {
-					$this->damageTaken[$abilityIdentifier][$type] *= .75;
-				}
-			}
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::SAP_SIPPER) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::GRASS] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::WATER_BUBBLE) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::FIRE] *= .5;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::FLUFFY) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::FIRE] *= 2;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::PRISM_ARMOR) {
-			$this->addToDamageTaken($ability);
-			foreach ($this->damageTaken[$abilityIdentifier] as $type => $multiplier) {
-				if ($multiplier > 1) {
-					$this->damageTaken[$abilityIdentifier][$type] *= .75;
-				}
-			}
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::PURIFYING_SALT) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::GHOST] *= .5;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::WELL_BAKED_BODY) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::FIRE] *= 0;
-			return;
-		}
-
-		if ($abilityIdentifier === AbilityIdentifier::EARTH_EATER) {
-			$this->addToDamageTaken($ability);
-			$this->damageTaken[$abilityIdentifier][TypeIdentifier::GROUND] *= 0;
-		}
-	}
-
-
-	/**
-	 * Add this ability to the matchups array.
-	 */
-	private function addToDamageTaken(ExpandedDexPokemonAbility $ability) : void
-	{
-		$this->abilities[] = [
-			'identifier' => $ability->identifier,
-			'name' => $ability->name,
-		];
-
-		$abilityIdentifier = $ability->identifier;
-		$this->damageTaken[$abilityIdentifier] = $this->damageTaken[self::NO_ABILITY];
 	}
 }
