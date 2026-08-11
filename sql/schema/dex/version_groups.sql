809d180855a811acf83df55e69d01f64f1414e77
Jesse Pirnat <jessepirnat@gmail.com>
2026-08-10 20:29:25 -0400

    Re-numbered versions and version groups to fit Colosseum and XD into the main sequence. In the process, removed a handful of obsolete data files.

--- sql/schema/dex/version_groups.sql
+++ sql/schema/dex/version_groups.sql
@@ -1,7 +1,7 @@
 create table if not exists `version_groups`
 (
 `id` tinyint unsigned not null,
-`identifier` varchar(4) not null,
+`identifier` varchar(9) not null,
 
 `generation_id` tinyint unsigned not null,
 `abbreviation` varchar(4) not null,
